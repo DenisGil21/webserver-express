7dc2f0df87e932481a71b38b688257d05fbeb84b
Denis Gil <denisgilsil.21@gmail.com>
2020-10-05 18:06:21 -0500

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        let salida = {
+            nombre: 'denis',
+            edad: '21',
+            url: req.url
+        }
+
+        res.write(JSON.stringify(salida));
+    })
+    .listen(8080);
+
+console.log('8080');
